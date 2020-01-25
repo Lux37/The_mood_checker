9f59df3c1337f636d0c553be27beede3d99aa3f5
Luca <lrazov7@gmail.com>
2020-01-25 16:50:31 +0100

    Initial commit

---
+++ mood.py
@@ -0,0 +1,14 @@
+mood = "happy"
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Cheer up, mate!")
+elif mood == "excited":
+    print("Yeah!")
+elif mood == "relaxed":
+    print("It is chill time.")
+else:
+    print("I don´t recognize this mood.")
